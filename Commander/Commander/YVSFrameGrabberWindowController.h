f5e993f9647065593230738b9c26ada3100e9009
Kevin Meaney <ktam@yvs.eu.com>
2013-05-29 14:03:46 +0100

    Project changes and new files for frame grabber command line tool configurator.

---
+++ Commander/Commander/YVSFrameGrabberWindowController.h
@@ -0,0 +1,13 @@
+//
+//  YVSFrameGrabberWindowController.h
+//  Commander
+//
+//  Created by Kevin Meaney on 29/05/2013.
+//  Copyright (c) 2013 Kevin Meaney. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface YVSFrameGrabberWindowController : NSWindowController
+
+@end
