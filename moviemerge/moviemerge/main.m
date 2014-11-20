40bcc4e469de70b3c03db9db75754b6bcb486e90
Kevin Meaney <ktam@yvs.eu.com>
2014-11-20 14:03:39 +0000

    Added a movie merge command line tool target

---
+++ moviemerge/moviemerge/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  moviemerge
+//
+//  Created by Kevin Meaney on 20/11/2014.
+//  Copyright (c) 2014 Kevin Meaney. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
